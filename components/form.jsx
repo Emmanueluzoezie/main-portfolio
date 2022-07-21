fae7ad64d071a4ae8640c357ede0083d0d70bbac
Emmanuel uzoezie <mac@macs-MacBook-Pro.local>
2022-07-21 12:59:28 +0100

    updating the form file

--- components/form.jsx
+++ components/form.jsx
@@ -1,5 +1,5 @@
 import { Button } from '@mui/material'
-import { useEffect, useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
 
 const Form = () => {
@@ -9,14 +9,17 @@ const Form = () => {
   function handleSubmit (e) {
     e.preventDefault()
 
-    emailjs.sendForm(process.env.REACT_APP_EMAILJS_SERVICE_ID,process.env.REACT_APP_EMAILJS_TEMPLATE_ID, form.current,process.env.REACT_APP_EMAILJS_PUBLIC_KEY)
+    emailjs.sendForm(process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID, form.current,process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY)
     .then(function(response) {
-      setMessage("successfully sent")
+      setMessage(<p className='text-2xl bg-green-500 animate-bounce mt-4 text-center'>Successfully sent</p>)
       return setTimeout(() => {
         setMessage("")
       }, 3000)
       }, function(err) {
-      console.log('FAILED...', err);
+        setMessage(<p className='text-2xl bg-red-500 animate-bounce mt-4 text-center'>Failed! please fill again</p>)
+        return setTimeout(() => {
+          setMessage("")
+        }, 5000)
     });
   }
 
@@ -26,17 +29,17 @@ const Form = () => {
         <h1 className='dark:text-white'>send your email</h1>
         <form className='flex flex-col w-full max-w-3xl px-8 md:px-none' onSubmit={handleSubmit} ref={form}>
             <label htmlFor="name" className='mt-4 dark:text-white font-bold pl-2 text-xl'>Name</label>
-            <input type="text" className='py-3 text-xl  font-semibold pl-2 shadow-xl mt-2 rounded-md' name="name" placeholder='Enter your name'/>
+            <input type="text" className='py-3 text-xl  font-semibold pl-2 shadow-xl mt-2 rounded-md' name="name" placeholder="Enter your name"/>
 
             <label htmlFor="name" className='mt-4 dark:text-white font-bold pl-2 text-xl'>Email</label>
-            <input type="text" className='py-3 text-xl  font-semibold pl-2 shadow-xl mt-2 rounded-md' name="user_email" placeholder='Enter your email'/>
+            <input type="text" className='py-3 text-xl  font-semibold pl-2 shadow-xl mt-2 rounded-md' name="user_email" placeholder="Enter your email"/>
 
             <label  htmlFor="name" className='mt-4 dark:text-white font-bold pl-2 text-xl'>Description</label>
-            <textarea className='py-3 shadow-xl font-semibold  pl-2 mt-2  text-xl rounded-md' name="" id="" cols="30" rows="4" placeholder='Enter your text'/>
+            <textarea className='py-3 shadow-xl font-semibold  pl-2 mt-2  text-xl rounded-md' name="message" id="" cols="30" rows="4" placeholder="Enter your text"/>
             <Button variant="contained" className='mt-4 font-bold text-xl py-2 shadow-xl dark:bg-gray-600 bg-blue-700' type="submit" value="Send">
                 submit
             </Button>
-            <p className='text-2xl bg-green-500 animate-bounce mt-4 text-center'>{message}</p>
+            <span>{message}</span>
         </form>
     </div> 
   )
