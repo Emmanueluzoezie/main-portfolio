29013dcdd113e1b7422bbeddb803912fea9af415
Emmanuel uzoezie <mac@macs-MacBook-Pro.local>
2022-07-22 21:45:25 +0100

    adding some text in the about page

--- components/about.jsx
+++ components/about.jsx
@@ -1,13 +1,13 @@
 export default function About() {
 
     return (
-        <div className="p-10 ">
+        <div className="py-10 px-4 ">
             <section className="">
                 <div className=" dark:text-white text-black md:px-16 text-center">
                     <h1 className="text-center text-2xl  font-bold font-mono">
                         Know Who <span> I Am </span>
                     </h1>
-                    <p className="text-lg mb-4"><span className="text-xl mr-1 font-semibold">I</span>am Emmanuel uzoezie, am a frontend developer.  having over 2+ years of experience in junior software Industry. Have work on some Frontend technologies, checkout the project page to see some of the projects have done so far <a href="https://emmanueluzoezie.bio.link/" className="hover:text-blue-600 hover:underline text-lg">social-links.</a></p>
+                    <p className="text-lg mb-4"><span className="text-xl mr-1 font-semibold">I</span>am Emmanuel uzoezie, am a frontend developer.  having over 2+ years of experience in junior software Industry. Have work on some Frontend technologies, checkout the project page to see some of the projects have done so far, go through my links to see more about me 👉 <a href="https://emmanueluzoezie.bio.link/" className="text-blue-600 underline text-lg">emmanuel-links.</a></p>
                     <h1 className="text-center text-2xl font-bold font-mono">
                         Hobbies
                     </h1>
