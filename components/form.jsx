ca93a9b22e985dcc5935d4708f516fc9abe5efea
Emmanuel uzoezie <mac@macs-MacBook-Pro.local>
2022-07-22 20:30:36 +0100

    chhange the whatsapp number

--- components/form.jsx
+++ components/form.jsx
@@ -26,7 +26,7 @@ const Form = () => {
 
   return (
     <div className='flex flex-col justify-center items-center mb-4'>
-        <h1 className='dark:text-white'>send your email</h1>
+        <h1 className='dark:text-white text-2xl font-bold capitalize'>send your email</h1>
         <form className='flex flex-col w-full max-w-3xl px-8 md:px-none' onSubmit={handleSubmit} ref={form}>
             <label htmlFor="name" className='mt-4 dark:text-white font-bold pl-2 text-xl'>Name</label>
             <input type="text" className='py-3 text-xl  font-semibold pl-2 shadow-xl mt-2 rounded-md' name="name" placeholder="Enter your name"/>
@@ -35,7 +35,7 @@ const Form = () => {
             <input type="text" className='py-3 text-xl  font-semibold pl-2 shadow-xl mt-2 rounded-md' name="user_email" placeholder="Enter your email"/>
 
             <label  htmlFor="name" className='mt-4 dark:text-white font-bold pl-2 text-xl'>Description</label>
-            <textarea className='py-3 shadow-xl font-semibold  pl-2 mt-2  text-xl rounded-md' name="message" id="" cols="30" rows="4" placeholder="Enter your text"/>
+            <textarea className='py-3 shadow-xl font-semibold  pl-2 my-3  text-xl rounded-md' name="message" id="" cols="30" rows="4" placeholder="Enter your text"/>
             <Button variant="contained" className='mt-4 font-bold text-xl py-2 shadow-xl dark:bg-gray-600 bg-blue-700' type="submit" value="Send">
                 submit
             </Button>
